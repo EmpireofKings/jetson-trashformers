6957e0e5e10bb7a9fd6e5ddea67a341e65183656
Michael Chacko <mikechacko10@gmail.com>
2017-07-25 10:37:13 -0700

    Commend KeyboardController.h

--- include/KeyboardController.h
+++ include/KeyboardController.h
@@ -9,14 +9,36 @@
 
 #include "ZigbController.h"
 
+/**
+ * Allows for human input to send zigb commands 
+ * through live input via console. WASD for movement etc..
+ * See cpp file for all controls
+ * 
+ */
+
 class KeyboardController{
     public:
+        /**
+         * Constructor for KeyboardController:
+         * Takes an instance of ZigController
+         */
         KeyboardController(ZigbController* zigb);
-
         virtual ~KeyboardController();
 
+        /**
+         * Sets up console for live input
+         */    
         void Init();
+
+        /**
+         * Runs a while loop to read input and send zigb commonds.
+         * (NOTE: THIS DOES NOT CREATE A SEPERATE THREAD)
+         */
         void RunInput();
+
+        /**
+         * Disables live console input
+         */
         void Terminate();
 
 
